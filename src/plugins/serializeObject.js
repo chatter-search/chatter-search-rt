7a75f93c4fe915d591b6a0b912a1b79b4449de0a
dmi3y <lapshukov@gmail.com>
2016-05-31 13:03:48 -0700

    using $.each vs lodash for serializeObject plugin

--- src/plugins/serializeObject.js
+++ src/plugins/serializeObject.js
@@ -4,14 +4,13 @@
  * have nothing to export just attaching itself to jquery
  */
 
-import {each} from 'lodash'
 import $ from 'jquery'
 
 $.fn.serializeObject = function () {
   var sdata = $(this).serializeArray()
   var data = {}
 
-  each(sdata, function (el) {
+  $.each(sdata, function (ix, el) {
     data[el.name] = el.value
   })
 
