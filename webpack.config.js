54f041aa9993c4fcc460e0d97e19f5d9159ae70a
dmi3y <lapshukov@gmail.com>
2016-06-03 01:20:15 -0700

    webpack config cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
-	resolve: {
-	    extensions: ['', '.js', '.jsx']
-  	},
-	entry: './src',
-	output: {
-		path: './build',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [{
-			test: /\.jsx?$/,
-			include: __dirname + '/src',
-			loader: 'babel'
-		}]
-	}
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  entry: './src',
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js/,
+      include: path.join(__dirname, 'src'),
+      loader: 'babel'
+    }]
+  }
 }
