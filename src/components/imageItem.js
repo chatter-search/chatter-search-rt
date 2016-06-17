9bacc2db79b0ad071214d8d7f4aa679dfea22681
dmi3y <lapshukov@gmail.com>
2016-06-17 10:27:59 -0700

    formatting

--- src/components/imageItem.js
+++ src/components/imageItem.js
@@ -3,17 +3,17 @@
  * @description Single image wraped up in the link, used for userTimelineContent
  */
 
- import React, {Component} from 'react'
+import React, {Component} from 'react'
 
 class ImageItem extends Component {
-	render () {
-		var data = this.props.data
-		return (
-			<a href={data.url}>
-				<img src={data.media_url_https} alt={data.text} />
-			</a>
-		)
-	}
+  render () {
+    var data = this.props.data
+    return (
+      <a href={data.url}>
+        <img src={data.media_url_https} alt={data.text} />
+      </a>
+    )
+  }
 }
 
 export default ImageItem
